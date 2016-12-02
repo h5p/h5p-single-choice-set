b27fbd4da343d1e44a38a4003028d4bbc938c2a7
Tom Arild Jakobsen <tajakobsen@gmail.com>
2016-12-02 11:28:35 +0100

    Handle illegal values of options.choices[i]. Relates to: #HFP-310

--- upgrades.js
+++ upgrades.js
@@ -6,7 +6,7 @@ H5PUpgrades['H5P.SingleChoiceSet'] = (function ($) {
       7: function (options, finished) {
         if(options.choices != undefined){
           for (var i = 0; i < options.choices.length; i++) {
-            if (options.choices[i].subContentId === undefined) {
+            if (options.choices[i] && options.choices[i].subContentId === undefined) {
               // NOTE: We avoid using H5P.createUUID since this is an upgrade script and H5P function may change in the
               // future
               options.choices[i].subContentId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
