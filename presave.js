43958ec330f717a822c7e8b74ce023d39e8bb27b
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-28 11:44:53 +0200

    Added JSDoc

--- presave.js
+++ presave.js
@@ -1,5 +1,12 @@
 var H5PPresave = H5PPresave || {};
 
+/**
+ * Resolve the presave logic for the content type Single Choice Set
+ *
+ * @param {object} content
+ * @param finished
+ * @constructor
+ */
 H5PPresave['H5P.SingleChoiceSet'] = function (content, finished) {
   var presave = H5PEditor.Presave;
   var score = 0;
@@ -16,6 +23,10 @@ H5PPresave['H5P.SingleChoiceSet'] = function (content, finished) {
     finished({maxScore: score});
   }
 
+  /**
+   * Check if required parameters is present
+   * @return {boolean}
+   */
   function isContentInValid() {
     return !presave.checkNestedRequirements(content, 'content.choices') || !Array.isArray(content.choices);
   }
