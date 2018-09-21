fe1bb98578ddf46e8979eaa93c2787f1718ae609
Paal Joergensen <pal.jorgensen@gmail.com>
2018-09-21 15:53:14 +0200

    Fix eslint error

--- upgrades.js
+++ upgrades.js
@@ -4,7 +4,7 @@ H5PUpgrades['H5P.SingleChoiceSet'] = (function () {
   return {
     1: {
       7: function (options, finished) {
-        if(options.choices != undefined){
+        if (options.choices != undefined) {
           for (var i = 0; i < options.choices.length; i++) {
             if (options.choices[i] && options.choices[i].subContentId === undefined) {
               // NOTE: We avoid using H5P.createUUID since this is an upgrade script and H5P function may change in the
