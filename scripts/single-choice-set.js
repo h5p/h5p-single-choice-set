c6799af7cb0a514d3d253bc5333a50f800ca10af
Ravi Majithia <ravikiran.majithia@joubel.com>
2021-11-04 16:44:33 +0100

    JI-2942 Add user response while saving content state - Because for IV and CP when user try to save the answer report don't   generate blank data

--- scripts/single-choice-set.js
+++ scripts/single-choice-set.js
@@ -14,6 +14,11 @@ H5P.SingleChoiceSet = (function ($, UI, Question, SingleChoice, SolutionView, Re
     // Extend defaults with provided options
     this.contentId = contentId;
     this.contentData = contentData;
+    /**
+     * The users input on the questions. Uses the same index as this.options.choices
+     * @type {number[]}
+     */
+    this.userResponses = [];
     Question.call(this, 'single-choice-set');
     this.options = $.extend(true, {}, {
       choices: [],
@@ -31,6 +36,7 @@ H5P.SingleChoiceSet = (function ($, UI, Question, SingleChoice, SolutionView, Re
     if (contentData && contentData.previousState !== undefined) {
       this.currentIndex = contentData.previousState.progress;
       this.results = contentData.previousState.answers;
+      this.userResponses = contentData.previousState.userResponse;
     }
     this.currentIndex = this.currentIndex || 0;
     this.results = this.results || {
@@ -48,12 +54,6 @@ H5P.SingleChoiceSet = (function ($, UI, Question, SingleChoice, SolutionView, Re
      */
     this.stopWatches = [];
     this.startStopWatch(this.currentIndex);
-
-    /**
-     * The users input on the questions. Uses the same index as this.options.choices
-     * @type {number[]}
-     */
-    this.userResponses = [];
 
     this.muted = (this.options.behaviour.soundEffectsEnabled === false);
 
@@ -832,7 +832,8 @@ H5P.SingleChoiceSet = (function ($, UI, Question, SingleChoice, SolutionView, Re
   SingleChoiceSet.prototype.getCurrentState = function () {
     return {
       progress: this.currentIndex,
-      answers: this.results
+      answers: this.results,
+      userResponse: this.userResponses
     };
   };
 
